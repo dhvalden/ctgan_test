b0df64e314702da35d78e7b7e3299e93cdd1c3c3
dhvalden <dhvalden@gmail.com>
2021-07-20 17:41:31 -0400

    changing the demo dataset and wrapping into main function

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 import warnings
-from sdv.demo import load_tabular_demo
+import pandas as pd
 from sdv.tabular import CTGAN
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
-data = load_tabular_demo('student_placements')
 
-print(data.head())
+def main():
+    data = pd.read_cvs('ESS8e02.1_F1.csv')
+    vars_to_use = ['idno',
+                   'cntry',
+                   'nwspol',
+                   'netusoft',
+                   'netustm',
+                   'ppltrst',
+                   'pplfair',
+                   'pplhlp',
+                   'polintr',
+                   'psppsgva',
+                   'actrolga',
+                   'psppipla',
+                   'cptppola',
+                   'trstprl',
+                   'trstlgl',
+                   'trstplc',
+                   'trstplt',
+                   'trstprt',
+                   'trstep',
+                   'trstun',
+                   'vote']
+    data = data[vars_to_use]
+    print(data.head())
+    model = CTGAN(
+        primary_key='idno'
+    )
+    model.fit(data)
+    new_data = model.sample(50000)
+    new_data.to_csv('synth_data.csv')
+    print(new_data.head())
 
-model = CTGAN()
-model.fit(data)
 
-new_data = model.sample(200)
-
-print(new_data.head())
+if __name__ == "__main__":
+    main()
